0d9f726770e538607039ba9caac142faa976206e
Ramaan09 <ramaan09@github.com>
2018-03-10 22:55:18 -0500

    Initial commit

---
+++ js/bucket.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("a.btn").click(function() {
+     $(".jumbotron").toggle("pulsate",1400); // jQuery UI effect 'pulsate'
+     $('h1').text('Hello there!');
+  });
+
+  $('.jumbotron').hide().delay(500).fadeIn(1400);
+
+  // jQuery UI Widgets
+
+  $( "#tabs" ).tabs();
+  $( "#accordion" ).accordion();
+
+});
